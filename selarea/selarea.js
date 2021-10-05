8dbb5e0b7df5aadb780de265da8c7933d912e6dd
James C <3661681+JamesMCo@users.noreply.github.com>
2021-10-05 17:03:44 +0100

    Add ability to show and hide combined xyz input boxes in selarea

--- selarea/selarea.js
+++ selarea/selarea.js
@@ -1,16 +1,21 @@
 ---
 ---
-const x1     = document.querySelector("#x1");
-const y1     = document.querySelector("#y1");
-const z1     = document.querySelector("#z1");
-const x2     = document.querySelector("#x2");
-const y2     = document.querySelector("#y2");
-const z2     = document.querySelector("#z2");
-const sel    = document.querySelector("#selector");
-const type   = document.querySelector("#type");
-const other  = document.querySelector("#other");
-const out    = document.querySelector("#out");
-const errors = document.querySelector("#errors");
+const combined_button  = document.querySelector("#combined_button");
+const separated_button = document.querySelector("#separated_button");
+const all1             = document.querySelector("#all1");
+const x1               = document.querySelector("#x1");
+const y1               = document.querySelector("#y1");
+const z1               = document.querySelector("#z1");
+const all2             = document.querySelector("#all2");
+const x2               = document.querySelector("#x2");
+const y2               = document.querySelector("#y2");
+const z2               = document.querySelector("#z2");
+const sel              = document.querySelector("#selector");
+const type             = document.querySelector("#type");
+const other            = document.querySelector("#other");
+const out              = document.querySelector("#out");
+const errors           = document.querySelector("#errors");
+let combined           = () => {return x1.parentElement.classList.contains("d-none");};
 
 selarea = (x1, y1, z1, x2, y2, z2, s, t, o) => {
     let origin = [Math.min(x1, x2), Math.min(y1, y2), Math.min(z1, z2)];
@@ -56,6 +61,63 @@ safe_selarea = () => {
     if (v_other !== "") { v_other = "," + v_other; }
 
     out.value = selarea(v_x1, v_y1, v_z1, v_x2, v_y2, v_z2, sel.value, v_type, v_other);
+}
+
+separate_combined = () => {
+    let coords1 = all1.value.split(" ");
+    x1.value = (coords1.length >= 1) ? coords1[0] : "";
+    y1.value = (coords1.length >= 2) ? coords1[1] : "";
+    z1.value = (coords1.length >= 3) ? coords1.slice(2).join(" ") : "";
+
+    let coords2 = all2.value.split(" ");
+    x2.value = (coords2.length >= 1) ? coords2[0] : "";
+    y2.value = (coords2.length >= 2) ? coords2[1] : "";
+    z2.value = (coords2.length >= 3) ? coords2.slice(2).join(" ") : "";
+}
+
+init_combined = () => {
+    if (combined()) {
+        return;
+    }
+
+    all1.value = [x1.value, y1.value, z1.value].join(" ");
+    all2.value = [x2.value, y2.value, z2.value].join(" ");
+
+    all1.parentElement.classList.remove("d-none");
+    all2.parentElement.classList.remove("d-none");
+    x1.parentElement.classList.add("d-none");
+    y1.parentElement.classList.add("d-none");
+    z1.parentElement.classList.add("d-none");
+    x2.parentElement.classList.add("d-none");
+    y2.parentElement.classList.add("d-none");
+    z2.parentElement.classList.add("d-none");
+
+    combined_button.classList.remove("btn-danger");
+    combined_button.classList.add("btn-secondary");
+    separated_button.classList.add("btn-danger");
+    separated_button.classList.remove("btn-secondary");
+}
+
+init_separated = () => {
+    if (!combined()) {
+        return;
+    }
+
+    separate_combined();
+
+    all1.parentElement.classList.add("d-none");
+    all2.parentElement.classList.add("d-none");
+    x1.parentElement.classList.remove("d-none");
+    y1.parentElement.classList.remove("d-none");
+    z1.parentElement.classList.remove("d-none");
+    x2.parentElement.classList.remove("d-none");
+    y2.parentElement.classList.remove("d-none");
+    z2.parentElement.classList.remove("d-none");
+
+    combined_button.classList.add("btn-danger");
+    combined_button.classList.remove("btn-secondary");
+    separated_button.classList.remove("btn-danger");
+    separated_button.classList.add("btn-secondary");
 }
 
 error = (m) => {
