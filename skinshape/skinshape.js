36fd28b113dbead3b14dd1b3fcdfe4bfb7a87b21
James C <3661681+JamesMCo@users.noreply.github.com>
2022-10-05 03:11:10 +0100

    Adjust image output to use img tag, rather than canvas

--- skinshape/skinshape.js
+++ skinshape/skinshape.js
@@ -2,16 +2,18 @@
 ---
 const widearm = /((custom|steve|s)_.*)|(.*_(custom|steve|s))\.png/;
 const slimarm = /((customSlim|alex|a|slim)_.*)|(.*_(customSlim|alex|a|slim))\.png/;
-const canvas  = document.querySelector("#canvas");
-const ctx     = canvas.getContext("2d");
+const output  = document.querySelector("#output");
 
 handle_file_change = async (event) => {
     // Called when a new file is selected in the input element
     let reader = new FileReader();
 
-    // Create an ImageBitmap that can be drawn to the canvas
-    let image = await createImageBitmap(event.target.files[0]);
-    
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(image, 0, 0);
+    // Read the file and convert it to base64 data:image/...
+    reader.readAsDataURL(event.target.files[0]);
+
+    reader.onload = (event) => {
+        // Called when the conversion finishes
+
+        output.src = event.target.result;
+    }
 }
